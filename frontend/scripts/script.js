7d4008ff31fa1631eecabe44e820612af78cf8b4
YerjanulyJanibek <93515401+YerjanulyJanibek@users.noreply.github.com>
2021-11-06 14:06:33 +0600

    Add files via upload

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,10 +1,18 @@
 
 
+// Тут через класс
+here = document.getElementsByClassName("here")
+here[1].innerHTML = "edednj"
+
+// Тут через айди
+first_here = document.getElementById("here1")
+first_here.innerHTML = y
+
 data = [
-		   ["Instagram", "Facebook", "Gmail"],
-		   ["adiina.y", "Adina Yerzhan", "erjankizi@gmail.com"],
-		   ["images/Instagram.png","images/Facebook.png","images/Gmail.png"]
-	   ]
+		["title1", "title2", "title3"],
+		["Text 1", "Text 2", "text 3"],
+		["images/bird.png", "images/dua_lipa.png", "images/brain.png"]
+		]
 
 for(var i = 0; i < 2; i++){
 	parent_box = document.getElementById("third_info")
@@ -14,9 +22,6 @@ for(var i = 0; i < 2; i++){
 }
 cards = document.getElementsByClassName("card")
 for(var i = 0; i < cards.length; i++){
-	cards[i].setAttribute("onmouseover", "change_style("+i+")")
-	cards[i].setAttribute("onmouseleave", "change_style2("+i+")")
-
 	header = cards[i].getElementsByClassName("card_header")[0]
 	header.innerHTML = data[0][i]
 	
@@ -25,32 +30,4 @@ for(var i = 0; i < cards.length; i++){
 
 	card_img = cards[i].getElementsByClassName("card_img")[0]
 	card_img.style.backgroundImage = "url("+data[2][i]+")"
-}
-
-function change_style(num) {
-	card_img=cards[num].getElementsByClassName('card_img')[0]
-	card_img.style.opacity=1;
-	hidden_text=card_img.getElementsByClassName("card_hidden_text")[0]
-	hidden_text.style.display = "block"
-}
-function change_style2(num) {
-	card_img=cards[num].getElementsByClassName('card_img')[0]
-	card_img.style.opacity=1;
-	hidden_text=card_img.getElementsByClassName("card_hidden_text")[0]
-	hidden_text.style.display = "none"
-}
-
-data2= [
-           ["Adina","Erjan","Sakan"]
-           ["18 mart" ,"1may", "3september"]
-           ["Doctor","businesman","doctor"]
-
-       ]
-fill_data()
-function fill_data(){
-	js_block=document.getElementsByClassName("js_block")[0]
-	for(var i=0;i<data2.length;j++){
-		div=js_block.createElement("div")
-		console.log(div)
-	}
 }
