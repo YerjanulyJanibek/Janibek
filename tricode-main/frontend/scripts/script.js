d08a4c8fd1c9d0e49aa8b986d89544b5a9246bd3
YerjanulyJanibek <93515401+YerjanulyJanibek@users.noreply.github.com>
2021-11-06 14:05:44 +0600

    Add files via upload

---
+++ tricode-main/frontend/scripts/script.js
@@ -0,0 +1,51 @@
+
+
+data = [
+		   ["Instagram", "Facebook", "Gmail"],
+		   ["adiina.y", "Adina Yerzhan", "erjankizi@gmail.com"],
+		   ["images/Instagram.png","images/Facebook.png","images/Gmail.png"]
+	   ]
+
+for(var i = 0; i < 3; i++){
+	parent_box = document.getElementById("third_info")
+	card = document.getElementsByClassName("card")[0]
+	card2 = card.cloneNode(true);
+	parent_box.appendChild(card2)	
+}
+cards = document.getElementsByClassName("card")
+for(var i = 0; i < cards.length; i++){
+	cards[i].setAttribute("onmouseover", "change_style("+i+")")
+	cards[i].setAttribute("onmouseleave", "change_style2("+i+")")
+
+	header = cards[i].getElementsByClassName("card_header")[0]
+	header.innerHTML = data[0][i]
+	
+	text = cards[i].getElementsByClassName("card_text")[0]
+	text.innerHTML = data[1][i]
+
+	card_img = cards[i].getElementsByClassName("card_img")[0]
+	card_img.style.backgroundImage = "url("+data[2][i]+")"
+}
+
+function change_style(num) {
+	card_img=cards[num].getElementsByClassName('card_img')[0]
+	card_img.style.opacity=1;
+	hidden_text=card_img.getElementsByClassName("card_hidden_text")[0]
+	hidden_text.style.display = "block"
+}
+function change_style2(num) {
+	card_img=cards[num].getElementsByClassName('card_img')[0]
+	card_img.style.opacity=1;
+	hidden_text=card_img.getElementsByClassName("card_hidden_text")[0]
+	hidden_text.style.display = "none"
+}
+
+
+
+function get_data(){
+	alert("yo")
+	response=fetch();
+	response.json().then(data =>{
+
+	})
+}
